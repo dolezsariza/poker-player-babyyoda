afac503f78380e94ea74408de434d77ab866d97f
Izabella Dolezsar <izabella.dolezsar@gmail.com>
2019-12-16 10:58:00 +0100

    switched return back to 0.

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,5 +1,6 @@
 package org.leanpoker.player;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 
 import java.util.Map;
@@ -9,7 +10,8 @@ public class Player {
     static final String VERSION = "1.0";
 
     public static int betRequest(JsonElement request) {
-        return 500;
+
+        return 0;
     }
 
     public static void showdown(JsonElement game) {
