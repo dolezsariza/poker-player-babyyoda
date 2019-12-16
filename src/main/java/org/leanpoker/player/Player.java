900dd881cd9f09885e2947035a4a764b514c7fbd
Izabella Dolezsar <izabella.dolezsar@gmail.com>
2019-12-16 14:59:46 +0100

    added cases

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -60,108 +60,152 @@ public class Player {
                 case 0: {
                     //pár
                     if (cards.isHolePair()) {
-                        if(current_buy_in < 200){
+                        if (current_buy_in < 200) {
                             return 500;
-                        }else{
+                        } else {
                             return toRaise;
                         }
                     } else if (cards.getHoleCardsValue() > 15) {
-                       if(current_buy_in < 200){
-                           return toRaise;
-                       }
+                        if (current_buy_in < 200) {
+                            return toRaise;
+                        }
                     } else return 0;
 
                 }
 
                 case 1: {
                     if (cards.isHolePair()
-                        || cards.isMixedPair()
-                        || cards.isTwoPairs()
-                        || cards.){
-
+                            || cards.isMixedPair()
+                            || cards.isTwoPairs()
+                            || cards.isDrill()) {
+                        if (current_buy_in < 300) {
+                            return toRaise;
+                        } else {
+                            return 0;
+                        }
                     }
+
+                    //TODO: if (cards.isPoker || cards.isFlush || cards.isStreak) { ALL IN }
+
                 }
 
-                case 2:
+                case 2: {
 
-                case 3:
+                    if (cards.isHolePair()
+                            || cards.isMixedPair()
+                            || cards.isTwoPairs()
+                            || cards.isDrill()) {
+                        if (current_buy_in < 300) {
+                            return toRaise;
+                        } else {
+                            return 0;
+                        }
+                    }
 
-                case 4:
+                    //TODO: if (cards.isPoker || cards.isFlush || cards.isStreak) { ALL IN }
+                }
 
-                    case 5:
+                case 3: {
+                    if (cards.isHolePair()
+                            || cards.isMixedPair()
+                            || cards.isTwoPairs()
+                            || cards.isDrill()) {
+                        if (current_buy_in < 300) {
+                            return toRaise;
+                        } else {
+                            return 0;
+                        }
+                    }
+
+                    //TODO: if (cards.isPoker || cards.isFlush || cards.isStreak) { ALL IN }
+
+                }
+                case 4: {
+                    if (cards.isHolePair()
+                            || cards.isMixedPair()
+                            || cards.isTwoPairs()
+                            || cards.isDrill()) {
+                        if (current_buy_in < 300) {
+                            return toRaise;
+                        } else {
+                            return 0;
+                        }
+                    }
+
+                    //TODO: if (cards.isPoker || cards.isFlush || cards.isStreak) { ALL IN }
+
+                }
+                case 5: {
                     if (current_buy_in < 200) {
                         return toRaise;
                     } else return 0;
+                }
             }
-
-
+            if (current_buy_in < 200) {
+                return current_buy_in;
+            } else return 0;
         }
-        if (current_buy_in < 200) {
-            return current_buy_in;
-        } else return 0;
+        return 0;
     }
-
-
-    public static void main(String[] args) {
-        String gameState = "{\n" +
-                "  \"players\":[\n" +
-                "    {\n" +
-                "      \"name\":\"Player 1\",\n" +
-                "     \"minimum_raise\": 240,\n" +
-                "      \"stack\":1000,\n" +
-                "      \"status\":\"active\",\n" +
-                "      \"bet\":0,\n" +
-                "      \"hole_cards\":[\n" +
-                "                {\n" +
-                "                    \"rank\": \"4\",\n" +
-                "                    \"suit\": \"hearts\"\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    \"rank\": \"K\",\n" +
-                "                    \"suit\": \"spades\"\n" +
-                "                }\n" +
-                "            ],\n" +
-                "      \"version\":\"Version name 1\",\n" +
-                "      \"id\":0\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"name\":\"Player 2\",\n" +
-                "      \"stack\":1000,\n" +
-                "      \"status\":\"active\",\n" +
-                "      \"bet\":0,\n" +
-                "      \"hole_cards\":[],\n" +
-                "      \"version\":\"Version name 2\",\n" +
-                "      \"id\":1\n" +
-                "    }\n" +
-                "  ],\n" +
-                "      \"in_action\":0,\n" +
-                "  \"tournament_id\":\"550d1d68cd7bd10003000003\",\n" +
-                "  \"game_id\":\"550da1cb2d909006e90004b1\",\n" +
-                "  \"round\":0,\n" +
-                "  \"bet_index\":0,\n" +
-                "  \"small_blind\":10,\n" +
-                "  \"orbits\":0,\n" +
-                "  \"dealer\":0,\n" +
-                "  \"community_cards\":[\n" +
-                "        {\n" +
-                "            \"rank\": \"4\",\n" +
-                "            \"suit\": \"spades\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"rank\": \"6\",\n" +
-                "            \"suit\": \"hearts\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"rank\": \"6\",\n" +
-                "            \"suit\": \"clubs\"\n" +
-                "        }\n" +
-                "    ],\n" +
-                "  \"current_buy_in\":0,\n" +
-                "  \"pot\":0\n" +
-                "}";
-        //betRequest(new JsonParser().parse(gameState));
-    }
-
+        public static void main (String[]args){
+            String gameState = "{\n" +
+                    "  \"players\":[\n" +
+                    "    {\n" +
+                    "      \"name\":\"Player 1\",\n" +
+                    "     \"minimum_raise\": 240,\n" +
+                    "      \"stack\":1000,\n" +
+                    "      \"status\":\"active\",\n" +
+                    "      \"bet\":0,\n" +
+                    "      \"hole_cards\":[\n" +
+                    "                {\n" +
+                    "                    \"rank\": \"4\",\n" +
+                    "                    \"suit\": \"hearts\"\n" +
+                    "                },\n" +
+                    "                {\n" +
+                    "                    \"rank\": \"K\",\n" +
+                    "                    \"suit\": \"spades\"\n" +
+                    "                }\n" +
+                    "            ],\n" +
+                    "      \"version\":\"Version name 1\",\n" +
+                    "      \"id\":0\n" +
+                    "    },\n" +
+                    "    {\n" +
+                    "      \"name\":\"Player 2\",\n" +
+                    "      \"stack\":1000,\n" +
+                    "      \"status\":\"active\",\n" +
+                    "      \"bet\":0,\n" +
+                    "      \"hole_cards\":[],\n" +
+                    "      \"version\":\"Version name 2\",\n" +
+                    "      \"id\":1\n" +
+                    "    }\n" +
+                    "  ],\n" +
+                    "      \"in_action\":0,\n" +
+                    "  \"tournament_id\":\"550d1d68cd7bd10003000003\",\n" +
+                    "  \"game_id\":\"550da1cb2d909006e90004b1\",\n" +
+                    "  \"round\":0,\n" +
+                    "  \"bet_index\":0,\n" +
+                    "  \"small_blind\":10,\n" +
+                    "  \"orbits\":0,\n" +
+                    "  \"dealer\":0,\n" +
+                    "  \"community_cards\":[\n" +
+                    "        {\n" +
+                    "            \"rank\": \"4\",\n" +
+                    "            \"suit\": \"spades\"\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "            \"rank\": \"6\",\n" +
+                    "            \"suit\": \"hearts\"\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "            \"rank\": \"6\",\n" +
+                    "            \"suit\": \"clubs\"\n" +
+                    "        }\n" +
+                    "    ],\n" +
+                    "  \"current_buy_in\":0,\n" +
+                    "  \"pot\":0\n" +
+                    "}";
+            //betRequest(new JsonParser().parse(gameState));
+        }
     public static void showdown(JsonElement game) {
     }
 }
