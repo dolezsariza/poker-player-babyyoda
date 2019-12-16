801eb1f47e53986669d6f494fa90bf7df440c32a
Abel Hodasz <hodabi@gmail.com>
2019-12-16 10:31:26 +0100

    trying things

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,16 +1,22 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
+import java.util.Set;
 
 public class Player {
 
     static final String VERSION = "1.0";
 
     public static int betRequest(JsonElement request) {
-        System.out.println(request.getAsString());
-        return 0;
+        JsonObject obj = request.getAsJsonObject();
+        Set<Map.Entry<String, JsonElement>> entries = obj.entrySet();
+        for (Map.Entry<String, JsonElement> entry: entries) {
+            System.out.println(entry.toString());
+        }
+        return 500;
     }
 
     public static void showdown(JsonElement game) {
