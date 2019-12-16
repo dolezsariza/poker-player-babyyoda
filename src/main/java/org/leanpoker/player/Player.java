70fbfd33848ea504b10a85261426c04b0a029f4a
Abel Hodasz <hodabi@gmail.com>
2019-12-16 10:14:29 +0100

    sout, what's going on?

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,6 +9,7 @@ public class Player {
     static final String VERSION = "1.0";
 
     public static int betRequest(JsonElement request) {
+        System.out.println(request.getAsString());
         return 0;
     }
 
